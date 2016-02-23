7b5ec7b3e7c576c70d3d68249fe5a73ffe37186d
lendale <nasibag1>
2016-02-23 17:57:42 +0800

    *Created BookApi class

---
+++ app/src/main/java/com/gabisan/midtermexam2/APIs/BookApi.java
@@ -0,0 +1,82 @@
+package com.gabisan.midtermexam2.APIs;
+
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+
+import com.gabisan.midtermexam2.Entities.Book;
+import com.gabisan.midtermexam2.Utils.HttpUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by daleg on 23/02/2016.
+ */
+public class BookApi {
+
+    public static final String BASE_URL = "http://joseniandroid.herokuapp.com";
+
+    public static final String PARAM_API = "api";
+    public static final String PARAM_BOOKS = "books";
+
+    private static final String B_ID = "_id";
+    private static final String B_TITLE = "title";
+    private static final String B_GENRE = "genre";
+    private static final String B_AUTHOR = "author";
+    private static final String B_ISREAD = "isRead";
+
+    public static List getBooks(Uri uri, @NonNull String requestMethod) {
+        String json = HttpUtils.getResponse(uri, requestMethod);
+
+        if (TextUtils.isEmpty(json)) {
+            return null;
+        }
+
+        //Parse the json response and convert it into a Book object
+        String id;
+        String title;
+        String genre;
+        String author;
+        Boolean isRead;
+
+        JSONArray jsonArray = new JSONArray();
+
+        JSONObject jsonObject = new JSONObject();
+
+        Book book;
+
+        List booksList = new ArrayList<Book>();
+
+        try {
+            for (int i = 0; i < jsonArray.length(); i++) {
+                jsonObject = jsonArray.getJSONObject(i);
+
+                id = jsonObject.getString(B_ID);
+                title = jsonObject.getString(B_TITLE);
+                genre = jsonObject.getString(B_GENRE);
+                author = jsonObject.getString(B_AUTHOR);
+                isRead = jsonObject.getBoolean(B_ISREAD);
+
+                book = new Book();
+
+                book.setmId(id);
+                book.setmTitle(title);
+                book.setmGenre(genre);
+                book.setmAuthor(author);
+                book.setmIsRead(isRead);
+
+                booksList.add(book);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return booksList;
+    }
+
+}
--- app/src/main/java/com/gabisan/midtermexam2/MainActivity.java
+++ app/src/main/java/com/gabisan/midtermexam2/MainActivity.java
@@ -1,15 +1,15 @@
 package com.gabisan.midtermexam2;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lvMainList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        lvMainList = (ListView) findViewById(R.id.lvMain_List);
     }
 
     @Override
